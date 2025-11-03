9093e3f12265ef27446d6e03cce9194d80924dbf
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2025-11-03 10:19:23 -0400

    Add WIP pirate game scene. Later move to new repo

--- Godot/Scripts/Runtime/[MyProject]/Constructs/Screens/Screen02_Pirates/PlayerShip.gd
+++ Godot/Scripts/Runtime/[MyProject]/Constructs/Screens/Screen02_Pirates/PlayerShip.gd
@@ -5,19 +5,22 @@
 # Class
 # ========================================
 
-class_name CornerUI
-extends Control
+class_name PlayerShip
+extends CharacterBody3D
 
 # ========================================
 # Constants
 # ========================================
+
+const GRAVITY: float = 9.8
+const ROTATION_OFFSET: float = PI  # 180 degrees in radians
 
 # ========================================
 # Exports
 # ========================================
 
 @export_group("Nodes")
-@export var richTextLabel: RichTextLabel
+@export var navigationAgent3D: NavigationAgent3D
 
 # ========================================
 # Signals
@@ -31,18 +34,48 @@ extends Control
 # Variables
 # ========================================
 
+var _input_vector : Vector2 = Vector2.ZERO
+
+# ========================================
+# Methods (DI)
+# ========================================
+
 # ========================================
 # Methods (Godot)
 # ========================================
 
 func _ready() -> void:
+	pass
 
-	# print("%s._ready()" % get_script().get_global_name())
+func _process(_delta: float) -> void:
+	process_input()
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func _exit_tree() -> void:
+	print("%s._exit_tree()" % get_script().get_global_name())
 	pass
 
 # ========================================
 # Methods (Custom)
 # ========================================
+
+func process_input() -> void:
+
+	_input_vector = Vector2.ZERO
+
+	# Get input direction
+	if Input.is_action_pressed("ui_accept"):
+		navigate_to_point()
+		pass
+
+func navigate_to_point () -> void:
+	print("ok")
+
+	navigationAgent3D.get_current_navigation_path()
+	pass;
 
 # ========================================
 # Event Handlers
